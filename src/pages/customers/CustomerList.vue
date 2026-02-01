b69e38fb4db7f25fc1daa7e3e03735bb6bc65bfd
Wdpf-66 <ccsl61588@gmail.com>
2026-02-01 11:54:43 +0600

    Customer Crud

---
+++ src/pages/customers/CustomerList.vue
@@ -0,0 +1,53 @@
+<template>
+  <div>
+      <h1>Customer List</h1>
+
+       <table>
+          <tr>
+              <th>Id</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Action</th>
+          </tr>
+          <tr v-for="customer in customers" :key="customer.id">
+              <td>{{ customer.id }}</td>
+              <td>{{ customer.name }}</td>
+              <td>{{ customer.email }}</td>
+              <td>
+                 <button>Edit</button>
+                 <button>Delete</button>
+              </td>
+              
+              
+          </tr>
+       </table>
+
+
+  </div>
+</template>
+
+<script  setup>
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+ const customers= ref([])
+
+ const fetchCustomers=()=>{
+   axios.get("http://localhost/wdpf-batch-66_class/Vue/laravel_reactapi/public/api/customers")
+   .then(res=>{
+      console.log(res.data.customers);
+      customers.value= res.data.customers;
+   })
+   .catch()
+ }
+
+ onMounted(()=>{
+    fetchCustomers();
+ })
+
+
+</script>
+
+<style>
+
+</style>
